0a0229a6198dcf679554193584232999470cfbf7
andyafene <andy.afene-mebaley@epitech.eu>
2019-05-29 15:00:43 +0200

    Get Youtube video

--- src/containers/app.js
+++ src/containers/app.js
@@ -20,8 +20,26 @@ class App extends Component {
 
     initMovies(){
         axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`)
-            .then(function (res) {
-                this.setState({ movieList: res.data.results.slice(1, 6), currentMovies: res.data.results[0] });
+            .then(function(res) {
+                this.setState({ movieList: res.data.results.slice(1, 6), currentMovies: res.data.results[0] }, function(){                   
+                    this.applyVideoToCurrent();
+                });
+            }.bind(this));
+    }
+
+    applyVideoToCurrent(){
+        axios.get(`${API_END_POINT}movie/${this.state.currentMovies.id}/videos?${API_KEY}&language=en-US`)
+             .then(function(res){
+                console.log('------------------');
+                console.log('tutu', res);
+                console.log('------------------');
+                const youtubeKey = res.data.results[0].key;
+                let newCurrentMovieState = this.state.currentMovies;
+                newCurrentMovieState.videoId = youtubeKey;
+                this.setState({currentMovies: newCurrentMovieState});
+                console.log('------------------');
+                console.log('', newCurrentMovieState);
+                console.log('------------------');
             }.bind(this));
     }
 
